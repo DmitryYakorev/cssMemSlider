a13ba15034d54ad55a6e09ec3f2cf206290ff914
Dmitry Yakorev <urakend@mail.ru>
2021-10-17 20:30:23 +0300

    feat:up3 2021-10-17 20:30:23

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -1,11 +1,13 @@
 let Position = 0;
 const PIC = document.querySelector('.pic');
+const TEXT = document.querySelector('.text_container');
 const LEFT = document.querySelector('.left');
 LEFT.addEventListener('click',(e) => {
   e.preventDefault;
   //console.log(LEFT,e,PIC);
   Position += 100;
   PIC.style.left = `${Position}px`;
+  TEXT.style.left = `${Position}px`;
   
 });
 const RIGHT = document.querySelector('.right');
@@ -14,4 +16,5 @@ RIGHT.addEventListener('click',(e) => {
   //console.log(RIGHT,e);
   Position -= 100;
   PIC.style.left = `${Position}px`;
+  TEXT.style.left = `${Position}px`;
 });
